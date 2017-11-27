029ba1629b424fdba3fc24778d6801467bed1a4c
elijah johnston <eli@millinno.com>
2017-11-27 14:31:54 -0200

    validations for the experience model

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,6 +2,11 @@ class Experience < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :features
 
-  validates :price, :name, :short_description, :long_description, :address, presence: true, allow_blank: false
-
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :title, presence: true, length: { minimum: 15 }
+  validates :short_description, presence:true, length: { maximum: 100 }
+  validates :long_description, presence:true, length: { minimum: 100 }, length: { minimum: 1000 }
+  validates :address, presence: true, allow_blank: false, length: { minimum: 10 }
+  validates :status, presence:true, default: false
+  validates :capacity, presence:true, numericality: { only_integer: true }
 end
