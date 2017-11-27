ffce5fe9d292af758557f14c7f01c772e64b9f0d
elijah johnston <eli@millinno.com>
2017-11-27 13:54:40 -0200

    simple validations for models

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,7 @@
 class Experience < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :features
+
+  validates :price, :name, :short_description, :long_description, :address, presence: true, allow_blank: false
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :experience
   belongs_to :user
+
+  validates :user, :experience, :start_date, :end_date, presence: true
 end
