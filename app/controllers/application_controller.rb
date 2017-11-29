96571a02d742cca15f13e9a13852c8034221ab3e
elijah johnston <eli@millinno.com>
2017-11-29 20:54:42 -0200

    initializing branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :set_vary_header
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -10,5 +11,12 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+  def set_vary_header
+    if request.xhr?
+      response.headers["Vary"] = "accept"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171129200129) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,11 +145,7 @@ ActiveRecord::Schema.define(version: 20171129200129) do
 
   create_table "reviews", id: :bigserial, force: :cascade do |t|
     t.text     "content"
-
-    t.integer  "experience_id"
-
-
-
+    t.bigint   "experience_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "rating"
