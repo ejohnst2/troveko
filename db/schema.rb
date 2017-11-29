b24ccdc19549122b607f5c0fcec5019d12707599
Geraldo Costa <gecosta7@gmail.com>
2017-11-29 18:02:44 -0200

    Add last_name to Trip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129195700) do
+ActiveRecord::Schema.define(version: 20171129200129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,6 +152,7 @@ ActiveRecord::Schema.define(version: 20171129195700) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "first_name"
+    t.string   "last_name"
     t.index ["experience_id"], name: "index_trips_on_experience_id", using: :btree
     t.index ["user_id"], name: "index_trips_on_user_id", using: :btree
   end
