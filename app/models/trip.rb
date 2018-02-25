54dee9511b17303eb1733252ec1a6599cb9e2222
elijah johnston <eli@millinno.com>
2018-02-25 15:04:18 +0800

    added logic to prevent overbooking - trying controller vs model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,10 +6,16 @@ class Trip < ApplicationRecord
 
   validates :user, :experience, :start_date, presence: true, allow_blank: false
   validate :start_date_cannot_be_in_the_past
+  validate :cannot_be_overbooking
 
   def start_date_cannot_be_in_the_past
     errors.add(:start_date, "Can't be in the past") if
       !start_date.blank? and start_date < Date.today
+  end
+
+  def cannot_be_overbooking
+    errors.add(:number_of_people, "You've exceeded the capacity for this weekend") if
+      number_of_people > (experience_id.capacity - start_date.number_of_people)
   end
 
 end
