74f28b144a51817a2f12327e65a475a8d744b959
elijah johnston <eli@millinno.com>
2018-02-28 22:41:48 +0800

    changed migration name of price column

--- db/migrate/20180227135252_change_price_cents_to_integer_in_experiences.rb
+++ db/migrate/20180227135252_change_price_cents_to_integer_in_experiences.rb
@@ -1,5 +1,5 @@
 class ChangePriceCentsToIntegerInExperiences < ActiveRecord::Migration[5.0]
   def change
-    change_column :experiences, :price, :integer
+    change_column :experiences, :price_cents, :integer
   end
 end
